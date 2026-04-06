88b5d825ca6a52bd25345d2fdf7c5d809ff00dde
Akhila26011142 <Akhila26011142@gmail.com>
2026-04-06 19:04:52 +0530

    Task2 completed

---
+++ task2_data_processing.py
@@ -0,0 +1,13 @@
+import os
+import glob
+import pandas as pd
+json_files = glob.glob("data/trends_*.json")
+if not json_files:
+    print("No JSON file found in data folder.")
+    exit()
+latest_file = max(json_files, key = os.path.getctime)
+df = pd.read_json(latest_file)
+print(f"Loaded {len(df)} stories from {latest_file}")
+print(f"After removing duplicates: {len(df)}")
+print(f"After removing nulls: {len(df)}")
+print(f"After removing low scores: {len(df)}")
